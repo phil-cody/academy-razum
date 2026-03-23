1bb45500977fee9a3160b4c3e08ddb6658f6697d
Dmitry <maiiamphiphi@gmail.com>
2026-03-23 19:03:15 +0700

    add script for speakers pop-up

--- scripts/popup-speakers.js
+++ scripts/popup-speakers.js
@@ -1,5 +1,25 @@
-// получить массив элементов класса pop-up
+const cardArr = Array.from(document.querySelectorAll(".pop-up"));
+let popupArr = document.querySelectorAll(".speakers__popup-card");
+let overlay = document.querySelector(".speakers__overlay");
 
-// получить массив элементов класса speakers__popup-card
+for (let i = 0; i < cardArr.length; i++) {
 
-// получить ссылку на оверлей для speakers
+  cardArr[i].addEventListener("click", (event) => {
+    if (event.target.classList.contains("slider-card__open-popup")) {
+      event.preventDefault();
+
+      popupArr[i].classList.add("popup-active");
+      overlay.classList.add("popup-active");
+
+      document.addEventListener("click", (event) => {
+        if (
+          event.target.classList.contains("slider-card__close-popup") ||
+          event.target.classList.contains("speakers__overlay")
+        ) {
+          popupArr[i].classList.remove("popup-active");
+          overlay.classList.remove("popup-active");
+        }
+      });
+    }
+  });
+}
