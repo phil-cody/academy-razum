0b1b3846b45f86b6d761d984e82c82b3cf793fa1
Dmitry <maiiamphiphi@gmail.com>
2026-03-24 17:10:07 +0700

    add script for modal form in header

---
+++ scripts/modal-form.js
@@ -0,0 +1,19 @@
+const modal = document.querySelector(".header__form-modal");
+const btn = document.querySelector(".header__contact button");
+const overlayModal = document.querySelector(".header__overlay");
+
+btn.addEventListener("click", (event) => {
+  console.log('поймал')
+  modal.classList.add("header__modal-active");
+  overlayModal.classList.add("header__modal-active");
+
+  document.addEventListener("click", (event) => {
+    if (
+      event.target.classList.contains('header__overlay') ||
+      event.target.classList.contains('header__modal-close')
+    ) {
+      modal.classList.remove("header__modal-active");
+      overlayModal.classList.remove("header__modal-active");
+    }
+  });
+});
