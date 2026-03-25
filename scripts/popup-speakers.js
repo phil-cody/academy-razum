c9f6517bbf097a4a195ccb23430e01cbc507df34
Dmitry <maiiamphiphi@gmail.com>
2026-03-25 20:11:41 +0700

    edit popup-speakers.js

--- scripts/popup-speakers.js
+++ scripts/popup-speakers.js
@@ -1,25 +1,33 @@
 const cardArr = Array.from(document.querySelectorAll(".pop-up"));
-let popupArr = document.querySelectorAll(".speakers__popup-card");
 let overlayPopup = document.querySelector(".speakers__overlay");
 
 for (let i = 0; i < cardArr.length; i++) {
-
   cardArr[i].addEventListener("click", (event) => {
     if (event.target.classList.contains("slider-card__open-popup")) {
       event.preventDefault();
 
-      popupArr[i].classList.add("popup-active");
-      overlayPopup.classList.add("popup-active");
+      const id = cardArr[i].dataset.popup;
+      if (!id) return;
 
-      document.addEventListener("click", (event) => {
-        if (
-          event.target.classList.contains("slider-card__close-popup") ||
-          event.target.classList.contains("speakers__overlay")
-        ) {
-          popupArr[i].classList.remove("popup-active");
-          overlayPopup.classList.remove("popup-active");
-        }
+      const popup = document.querySelector(`.speakers__popup-card[data-popup="${id}"]`);
+      if (!popup) return;
+
+      document.querySelectorAll('.popup-active').forEach(el => {
+        el.classList.remove('popup-active');
       });
+
+      popup.classList.add('popup-active');
+      overlayPopup.classList.add('popup-active');
     }
   });
 }
+
+document.addEventListener('click', event => {
+  if (event.target.classList.contains('slider-card__close-popup') || event.target.classList.contains('speakers__overlay')) {
+    document.querySelectorAll('.popup-active').forEach(el => {
+      el.classList.remove('popup-active');
+    });
+
+    overlayPopup.classList.remove('popup-active');
+  }
+});
